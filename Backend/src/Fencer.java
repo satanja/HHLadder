01f4dee69821a61704e24a30ee89805d7c143336
satanja <s.a.tanja@student.tue.nl>
2018-10-14 16:19:04 +0200

    Back: Add ELOFencer interface to Fencer

--- Backend/src/Fencer.java
+++ Backend/src/Fencer.java
@@ -1,21 +1,67 @@
-public class Fencer {
+/**
+ * @author S.A. Tanja
+ *
+ *
+ */
+public class Fencer implements ELOFencer {
 
+    // The name of the fencer
     private String name;
 
+    // The weapon the fencer uses
     private Weapon weapon;
+
+    // The match making rating (mmr) of the fencer
+    private int mmr;
+
+    // The number of bouts the fencer fenced
+    private int boutsFenced;
 
     public Fencer(String name, Weapon weapon) {
         this.name = name;
         this.weapon = weapon;
+        // mmr start point is 1000
+        mmr = 1000;
+        boutsFenced = 0;
     }
 
+    /**
+     * Gets the name of the fencer
+     * @return {@code this.name}
+     */
     public String getName() {
         return name;
     }
 
 
+    /**
+     * Gets the weapon of the fencer
+     * @return {@code this.weapon}
+     */
     public Weapon getWeapon() {
         return weapon;
+    }
+
+
+    @Override
+    public int getBoutsFenced() {
+        return boutsFenced;
+    }
+
+
+    @Override
+    public void increaseBoutsFenced() {
+        boutsFenced++;
+    }
+
+    @Override
+    public int getMMR() {
+        return 0;
+    }
+
+    @Override
+    public void setMMR(int newMMR) {
+        this.mmr = newMMR;
     }
 
     public static void main(String[] args) {
