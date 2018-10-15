68bfbb9adfb712ff6265a8e505318d838b0e08cc
Stefan Tanja <s.a.tanja@student.tue.nl>
2018-10-15 12:20:28 +0200

    Back: Add Ladder sets initial MMR fencer

--- Backend/src/Ladder.java
+++ Backend/src/Ladder.java
@@ -15,10 +15,13 @@ public class Ladder {
     // The weapon of the ladder
     private Weapon weapon;
 
+    // The ELO system of this ladder
+    private ELOManager eloManager;
 
-    public Ladder(Weapon weapon) {
+    public Ladder(Weapon weapon, ELOManager eloManager) {
         this.weapon = weapon;
         fencers = new ArrayList<>();
+        this.eloManager = eloManager;
     }
 
     /**
@@ -27,11 +30,14 @@ public class Ladder {
      *     <li>The {@code Fencer} uses the same weapon as the {@code Ladder}</li>
      *     <li>There does not already exist a {@code Fencer} with the same name</li>
      * </ul>
+     * Also sets the initial MMR of the fencer
      * @return true when the {@code fencer} has the same weapon and a fencer with the same name does not exist.
      */
     public boolean addFencer(Fencer fencer) {
         if (fencerHasSameWeapon(fencer) && !containsFencer(fencer)) {
             fencers.add(fencer);
+            int initialMMR = eloManager.getInitialMMR(fencers.indexOf(fencer));
+            fencer.setMMR(initialMMR);
             return true;
         }
         return false;
