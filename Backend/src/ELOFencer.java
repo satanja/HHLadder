7259d8e0886bded4ace205f71c5afb570a7f5137
satanja <s.a.tanja@student.tue.nl>
2018-10-15 14:12:38 +0200

    Back: Remove redundant public accessor

--- Backend/src/ELOFencer.java
+++ Backend/src/ELOFencer.java
@@ -7,24 +7,24 @@ public interface ELOFencer {
      * Gets the number of bouts fenced
      * @return The number of bouts fenced
      */
-    public int getBoutsFenced();
+    int getBoutsFenced();
 
     /**
      * Increase the number of bouts fenced by one
      */
-    public void increaseBoutsFenced();
+    void increaseBoutsFenced();
 
     /**
      * Get the current MMR of the fencer
      * @return The MMR of the fencer
      */
-    public int getMMR();
+    int getMMR();
 
     /**
      * Set the MMR of the fencer to its new MMR
      * @param newMMR The new MMR of the fencer
      */
-    public void setMMR(int newMMR);
+    void setMMR(int newMMR);
 
 
 }
