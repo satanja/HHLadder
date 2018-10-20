3dae2891155152fba824c7f0ee3c82999b644104
satanja <s.a.tanja@student.tue.nl>
2018-10-20 15:34:13 +0200

    Back: Add simple test for SimpleELOManager

---
+++ Backend/test/ELO/SimpleELOManagerTest.java
@@ -0,0 +1,28 @@
+package ELO;
+
+import static org.junit.Assert.*;
+import Records.Fencer;
+
+import Records.Match;
+import org.junit.Before;
+import org.junit.Test;
+import Weapon.*;
+
+
+
+public class SimpleELOManagerTest extends ELOManagerTest {
+
+
+    @Override
+    public void setUpInstance() {
+        instance = new SimpleELOManager();
+    }
+
+    @Test
+    public void match1() {
+        setUpMatch(14, 15);
+        checkMMR(1400, 600);
+    }
+
+
+}
