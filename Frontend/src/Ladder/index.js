1c2e85df09ea1bed2f06e3b632228f6156e7a581
satanja <s.a.tanja@student.tue.nl>
2018-11-19 22:39:29 +0100

    Front: Add bootstrap Ladder

--- Frontend/src/Ladder/index.js
+++ Frontend/src/Ladder/index.js
@@ -1,38 +1,55 @@
 import React from 'react';
+import { Table } from 'react-bootstrap';
+import styled from  'styled-components';
 
 /**
  * Simple table with all the fencers of a specific ladder
  * Author: S.A. Tanja
  * Props:
- *  - fencers: array of fencer objects
- *  - isHiddenMMR: whether to draw the MMR in the table
+ *  - Response JSON message from the backend
+ *    - fencers: array of fencer objects 
+ *    - isHiddenMMR: whether to draw the MMR in the table
+ *  - name The name of the ladder   
  */
 const Ladder = props => {
   const { fencers, isHiddenMMR } = props.response;
   return (
-    <table>
-      <thead>
-        <tr>
-          <th>Position</th>
-          <th>Name</th>
-          {isHiddenMMR ? null : <th>MMR</th>}
-        </tr>
-      </thead>
-      <tbody>
-        {fencers.map((fencer, index) => {
-          const position = `${index + 1}.`;
-          const { name, MMR } = fencer;
-          return(
-            <tr key={position}>
-              <td>{position}</td>
-              <td>{name}</td>
-              {isHiddenMMR ? null : <td>{MMR}</td>}
-            </tr>
-          )
-        })}
-      </tbody>
-      </table>
+    <TableWindow>
+      <Table striped bordered condensed hover>
+        <Caption>{props.name}</Caption>
+        <thead>
+          <tr>
+            <th>Position</th>
+            <th>Name</th>
+            {isHiddenMMR ? null : <th>MMR</th>}
+          </tr>
+        </thead>
+        <tbody>
+          {fencers.map((fencer, index) => {
+            const position = `${index + 1}.`;
+            const { name, MMR } = fencer;
+            return(
+              <tr key={position}>
+                <td>{position}</td>
+                <td>{name}</td>
+                {isHiddenMMR ? null : <td>{MMR}</td>}
+              </tr>
+            )
+          })}
+        </tbody>
+        </Table>
+      </TableWindow>
   )
 }
 
+const TableWindow = styled.div`
+  width: 90%;
+  margin: 0 auto;
+`;
+
+const Caption = styled.caption`
+  text-align: center;
+  font-weight: bold;
+  color: #000000;
+`;
 export default Ladder;
