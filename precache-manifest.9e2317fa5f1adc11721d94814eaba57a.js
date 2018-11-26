284ebde7e593c8bf6522c374149cc6b020e43ca9
satanja <s.a.tanja@student.tue.nl>
2018-11-26 22:21:28 +0100

    Updates

---
+++ precache-manifest.9e2317fa5f1adc11721d94814eaba57a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "86b438f98cb38a90339a",
+    "url": "/HHLadder/static/js/runtime~main.86b438f9.js"
+  },
+  {
+    "revision": "057c77ad9cd0330826e1",
+    "url": "/HHLadder/static/js/main.057c77ad.chunk.js"
+  },
+  {
+    "revision": "b8ffecd7f1f1b114ff37",
+    "url": "/HHLadder/static/js/1.b8ffecd7.chunk.js"
+  },
+  {
+    "revision": "057c77ad9cd0330826e1",
+    "url": "/HHLadder/static/css/main.e90684ee.chunk.css"
+  },
+  {
+    "revision": "29bdbcc6f577224bec92dcea8a21282a",
+    "url": "/HHLadder/index.html"
+  }
+];
